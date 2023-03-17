65172b6f3176c75bc77d085fcc0560c6bdb7e08a
Ty <Ty@Tys-Macbook.lan>
2023-03-16 22:26:55 -0400

    greetings complete

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/greeting', (req, res) => {
-    res.send("Hello, stranger");
+app.get('/greeting/:name', (req, res) => {
+    res.send(`What's up, ${req.params.name}`);
 })
+
+
 
 //keep this at the bottom of our file
 app.listen(3000, () => {
